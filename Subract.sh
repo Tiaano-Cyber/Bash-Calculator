03bfcdf201461b47e28c8d67bce321e70e58bc4b
Tiaano-Cyber <professionaledits0@gmail.com>
2025-02-19 20:38:17 +0530

    Create Subract.sh
    New Subract Option

---
+++ Subract.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+figlet "Subraction"| lolcat
+
+#Ask User To Enter First Number
+
+echo Enter First Number To Subract
+read num1
+
+#Ask User To Enter Second Number
+
+echo Enter Second Number To Subract
+read num2
+
+result=$((num1 - num2))
+
+#Saving History
+echo "$(date) - The subraction of $num1 and $num2 is: $result" >> history5.txt
+
+#Answer
+echo The Subraction Of $num1 And $num2 Is :$result
+
+sleep 1m
+
+echo $(clear && bash)
+
+
+
+
+
+
+
+
